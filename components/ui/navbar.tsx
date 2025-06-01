0a0af4d6e5ca7bf4e2f54ba558db36bc45f96ece
Tanujeet <singhtanujeet@gmail.com>
2025-06-01 08:20:58 +0530

    navbar

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -1,21 +1,22 @@
 import Link from 'next/link'
 import React from 'react'
 
-const navbar = () => {
+const Navbar = () => {
   return (
-      <nav className='navbar'>
-          <Link href='/'>
-              <div className='flex items-center gap-2.5 cursor-pointer'>
-                  <img src="/images/logo.sv" alt="logo" width={46} height={44}/>
-              </div>
+    <nav className="navbar">
+      <Link href="/">
+        <div className="flex items-center gap-2.5 cursor-pointer">
+          <img src="/images/logo.svg" alt="logo" width={46} height={44} />
+        </div>
       </Link>
-      <div className='flex items-center gap-8 '>
+      <div className="flex items-center gap-8 ">
         <p>Home</p>
+        <p>Companions</p>
         <p>My Journey</p>
         <p>Sign in</p>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default navbar
+export default Navbar;
