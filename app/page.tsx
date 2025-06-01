2737d08d43774e7700bc80587a515cb15c4b8c87
Tanujeet <singhtanujeet@gmail.com>
2025-06-01 09:35:21 +0530

    implemented companion Card

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import CompanionList from "@/components/CompanionList";
+import CompanionList from "@/components/ui/CompanionList";
 import CompanionCard from "@/components/ui/CompanionCard";
 import CTA from "@/components/ui/CTA";
 
@@ -8,9 +8,30 @@ const page = () => {
       <h1>
         Popular Companions
         <section className="home-section">
-          <CompanionCard />
-          <CompanionCard />
-          <CompanionCard />
+          <CompanionCard
+            id="123"
+            name="Neura the Brainy Explorer"
+            topic="Neural Network of the Brain"
+            subject="science"
+            duration={45}
+            color="#ffda6e"
+          />
+          <CompanionCard
+            id="456"
+            name="Countsy the Number Wizard"
+            topic="Derivatives & Integrals"
+            subject="Maths"
+            duration={30}
+            color="#e5d0ff"
+          />
+          <CompanionCard
+            id="789"
+            name="Verba the Vocabulary Builder"
+            topic="English Literature"
+            subject="Language"
+            duration={30}
+            color="#BDE7FF"
+          />
         </section>
         <section className="home-section">
           <CompanionList />
