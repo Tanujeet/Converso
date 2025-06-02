003ea992022e5f6b6f5efbd3cce5c0eac0384815
Tanujeet <singhtanujeet@gmail.com>
2025-06-02 09:25:36 +0530

    implemented subscription

--- app/subsciption/page.tsx
+++
@@ -1,7 +0,0 @@
-
-
-const subsciption = () => {
-  return <div>page</div>;
-};
-
-export default subsciption
---
+++ app/subscription/page.tsx
@@ -0,0 +1,11 @@
+import { PricingTable } from "@clerk/nextjs";
+
+const subsciption = () => {
+  return (
+    <div>
+      <PricingTable />
+    </div>
+  );
+};
+
+export default subsciption;
