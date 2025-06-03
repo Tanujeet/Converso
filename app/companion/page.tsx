a19c9a49307f79944a2f00ca7972d81bacaef935
Tanujeet <singhtanujeet@gmail.com>
2025-06-03 07:52:23 +0530

    implemented companion page

--- app/companion/page.tsx
+++ app/companion/page.tsx
@@ -1,3 +1,4 @@
+import CompanionCard from "@/components/ui/CompanionCard";
 import { getAllCompanions } from "@/lib/actions/companion.action";
 
 const CompanionLibrary = async ({ searchParams }: SearchParams) => {
@@ -7,7 +8,19 @@ const CompanionLibrary = async ({ searchParams }: SearchParams) => {
 
   const companions = await getAllCompanions({ subject, topic });
 
-  return <div>page</div>;
+  return (
+    <main>
+      <section className=" flex justify-center gap-4 max-sm:flex-col">
+        <h1>Companion Library</h1>
+        <div className="flex gap-4">Filters</div>
+      </section>
+      <section className="companions-grid">
+        {companions.map((companion) => (
+          <CompanionCard key={companion.id} {...companion} />
+        ))}
+      </section>
+    </main>
+  );
 };
 
 export default CompanionLibrary;
