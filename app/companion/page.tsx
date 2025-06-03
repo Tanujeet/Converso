37b057c004bfa00e0c520dea01d506663bd1451a
Tanujeet <singhtanujeet@gmail.com>
2025-06-03 07:48:52 +0530

    implemented companion page

--- app/companion/page.tsx
+++ app/companion/page.tsx
@@ -1,5 +1,13 @@
-const page = () => {
+import { getAllCompanions } from "@/lib/actions/companion.action";
+
+const CompanionLibrary = async ({ searchParams }: SearchParams) => {
+  const filters = await searchParams;
+  const subject = filters.subject ? filters.subject : "";
+  const topic = filters.topic ? filters.topic : "";
+
+  const companions = await getAllCompanions({ subject, topic });
+
   return <div>page</div>;
 };
 
-export default page;
+export default CompanionLibrary;
