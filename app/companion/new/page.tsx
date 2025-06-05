6bc8ecf8a3d5177cd1fb4cf3d17f9a4df8a288a4
Tanujeet <singhtanujeet@gmail.com>
2025-06-05 10:34:02 +0530

    implemented  some minor changes

--- app/companion/new/page.tsx
+++ app/companion/new/page.tsx
@@ -15,7 +15,7 @@ const NewCompanion = async () => {
   const canCreateCompanion = await newCompanionPermissions();
 
   return (
-    <main className="w-full md:w-2/3 lg:w-1/3 mx-auto flex items-center justify-center min-h-screen">
+    <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
       {canCreateCompanion ? (
         <article className="w-full gap-4 flex flex-col">
           <h1 className="text-2xl font-bold">Companion Builder</h1>
